a43c56160a0ee31520bd8ede391df1f39ffbe248
comverser <hyunduk0206@gmail.com>
2022-06-16 02:59:55 +0000

    Format svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,10 @@ const config = {
 		vite: {
 			server: {
 				hmr: {
-					clientPort: process.env.HMR_HOST ? 443: 3000,
-					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+					clientPort: process.env.HMR_HOST ? 443 : 3000,
+					host: process.env.HMR_HOST
+						? process.env.HMR_HOST.substring('https://'.length)
+						: 'localhost'
 				}
 			}
 		}
