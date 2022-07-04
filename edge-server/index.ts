d4812eaba21ecacd6da39a28ac74f70f50a08bcc
Hyunduk Shin <hyunduk0206@gmail.com>
2022-07-04 10:22:37 +0900

    Develop Yeelight socket

--- edge-server/index.ts
+++ edge-server/index.ts
@@ -5,18 +5,27 @@ import util from 'util';
 import { exec } from 'child_process';
 import dotenv from 'dotenv';
 
+import { Client } from 'yeelight-node';
+
 dotenv.config();
 
 const port = process.env.PORT || 3080;
 const app: Express = express();
 const server = createServer(app);
 const execPromise = util.promisify(exec);
+const smartLight = new Client();
 
 const io = new Server(server, {
 	cors: {
 		origin: '*',
 		methods: ['GET', 'POST']
 	}
+});
+
+let yeelight: any;
+smartLight.bind((light: any) => {
+	yeelight = light;
+	console.debug(light);
 });
 
 io.on('connection', (socket) => {
@@ -33,6 +42,25 @@ io.on('connection', (socket) => {
 			return;
 		}
 		console.debug('stdout:', stdout);
+	});
+
+	socket.on('startYeelight', async () => {
+		yeelight.set_power('on');
+		// yeelight.set_rgb(color);
+		yeelight.get_prop('bright').then((data: any) => console.debug(data));
+		// yeelight.set_power('off');
+	});
+
+	socket.on('setYeelight', (color: [number, number, number]) => {
+		yeelight.set_rgb(color);
+	});
+
+	socket.on('endYeelight', () => {
+		yeelight.get_prop('bright').then((data: any) => console.debug(data));
+		yeelight.set_power('off');
+		// setTimeout(() => {
+		// 	yeelight.closeConnection();
+		// }, 1000);
 	});
 
 	socket.on('disconnect', (socket) => {
---
+++ src/routes/apps/therapy/index.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { startSocket, endSocket } from '$root/lib/edgeServer';
+	import { onDestroy } from 'svelte';
+
+	const socket = startSocket();
+
+	const handleClick = (event: PointerEvent) => {
+		console.log(event.target.innerText);
+
+		switch (Number(event.target.innerText)) {
+			case 1:
+				socket.emit('startYeelight');
+				break;
+			case 2:
+				socket.emit('setYeelight', [255, 0, 0]);
+				break;
+			case 3:
+				socket.emit('setYeelight', [0, 255, 0]);
+				break;
+			case 4:
+				socket.emit('endYeelight');
+				break;
+
+			default:
+				break;
+		}
+	};
+
+	onDestroy(() => {
+		endSocket(socket);
+	});
+</script>
+
+<div>
+	<button on:click={handleClick}>1</button>
+	<button on:click={handleClick}>2</button>
+	<button on:click={handleClick}>3</button>
+	<button on:click={handleClick}>4</button>
+</div>
+
+<style>
+	div {
+		height: 100vh;
+	}
+	button {
+		width: 100%;
+		height: 20%;
+	}
+</style>
